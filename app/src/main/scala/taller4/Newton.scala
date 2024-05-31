0c9f23afecaf8322e384efb7840927cc2270b969
Rojasjuan08-2 <juan.qrojas9.5@gmail.com>
2024-05-31 16:26:44 -0500

    Implementación de función evaluar

--- app/src/main/scala/taller4/Newton.scala
+++ app/src/main/scala/taller4/Newton.scala
@@ -11,5 +11,17 @@ case class Expo(e1:Expr, e2:Expr) extends Expr
 case class Logaritmo(e1:Expr) extends Expr
 
 class Newton {
-
+  def evaluar(f: Expr, a: Atomo, v: Double): Double = f match {
+    case Numero(d) => d
+    case Atomo(x) =>
+      if (x == a.x) v
+      else throw new IllegalArgumentException(s"Se encontró un átomo inesperado $x")
+    case Suma(e1, e2) => evaluar(e1, a, v) + evaluar(e2, a, v)
+    case Resta(e1, e2) => evaluar(e1, a, v) - evaluar(e2, a, v)
+    case Prod(e1, e2) => evaluar(e1, a, v) * evaluar(e2, a, v)
+    case Div(e1, e2) => evaluar(e1, a, v) / evaluar(e2, a, v)
+    case Expo(e1, e2) => math.pow(evaluar(e1, a, v), evaluar(e2, a, v))
+    case Logaritmo(e1) => math.log(evaluar(e1, a, v))
+    case _ => throw new IllegalArgumentException("No se conoce el tipo de expresión")
+  }
 }
